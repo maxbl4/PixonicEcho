46d1d7eaf3f337a4238d26a972272aa4675fd2ac
maxbl4 <maxbl4@gmail.com>
2017-03-22 21:08:52 +0300

    Added comments to Settings

--- PixonicEcho/Settings.cs
+++ PixonicEcho/Settings.cs
@@ -2,6 +2,23 @@ namespace PixonicEcho
 {
     class Settings
     {
+        /// Типичные варианты настроек:
+        ///  - Посмотреть как два клиента говорят между собой:
+        /// ClientMessageIntervalMs = 1000
+        /// ClientsPerRoom = 2
+        /// Rooms = 1
+        /// PrintMessagesToConsole = true
+        /// Запускать с ключом benchmark. Будет создана одна комната и два клиента, каждое
+        /// их сообщение будет выводиться на консоль
+        /// 
+        ///  - Проверить прозводительность сервера
+        /// ClientMessageIntervalMs = 10
+        /// ClientsPerRoom = 10
+        /// Rooms = 10
+        /// PrintMessagesToConsole = false
+        /// Будет создано 10 комнат в каждой 10 клиентов.
+
+
         public const int ClientMessageIntervalMs = 1;
         public const int RoomLifeTimeoutMs = 5000;
         public const int ClientLifeMs = 500;
